6a95627569020342dcb9e5a7e2667f3cd4866b8d
+ <+>
2021-09-01 08:10:48 -0500

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,21 @@
+function receivesAFunction(callback) {
+    callback();
+}
+
+function returnsANamedFunction() {
+    const food = function chicken() {
+       
+    }
+    return food;
+}
+
+
+function returnsAnAnonymousFunction() {
+    const createPokemon = function() {
+        //anonymous function
+    l
+    }
+    return function() {
+        
+    };
+}
